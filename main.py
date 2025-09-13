fd79df51a3e2a4e14e526b4395af1520736069af
tmdtlr <tmdtlr4766@naver.com>
2025-09-13 17:04:22 +0900

    로컬 변경사항 저장

--- main.py
+++ main.py
@@ -11,7 +11,29 @@ def print_hi(name):
 
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm')#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
 
 # https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
 
